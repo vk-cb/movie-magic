1a85aa4983dcc3e48297f24295e50b7e16d72a64
Vicky Kumar Gupta <vickygupta031@gmail.com>
2025-01-17 22:42:54 +0530

    add mongodb to connect database

---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+let initialized = false;
+export const connect = async () => {
+  mongoose.set('strictQuery', true);
+  if (initialized) {
+    console.log('MongoDB already connected');
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'movie-magic',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    initialized = true;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.log('MongoDB connection error:', error);
+  }
+};
