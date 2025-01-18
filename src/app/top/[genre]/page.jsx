140fc65112ae69d28085e7b3e58cb3ec2925902c
Vicky Kumar Gupta <vickygupta031@gmail.com>
2025-01-18 21:45:40 +0530

    add api and result page

--- src/app/top/[genre]/page.jsx
+++ src/app/top/[genre]/page.jsx
@@ -1,14 +1,18 @@
 import Results from '@/components/Results';
 const API_KEY = process.env.API_KEY;
-export default async function Home() {
+export default async function Home({ params }) {
+  const { genre } = await params;
   const res = await fetch(
-    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/3${
+      genre === 'rated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
   );
   const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
   const results = data.results;
+
   return (
     <div>
       <Results results={results} />
